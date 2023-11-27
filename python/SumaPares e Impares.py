34d98d78d3f742bb746ccdd0bb54500a753f469a
guillegafas <116709250+guillegafas@users.noreply.github.com>
2023-11-27 13:54:11 +0100

    Add files via upload

---
+++ python/SumaPares e Impares.py
@@ -0,0 +1,14 @@
+SumaPares=0;
+SumaImpares=0;
+i=0;
+for i in range(1, 100):
+    if i % 2 == 0:
+        SumaPares=SumaPares + i
+    else:
+        SumaImpares=SumaImpares + i
+
+print(SumaPares)
+print(SumaImpares)
+
+
+
---
+++ python/ejercicios_for.py
@@ -0,0 +1,41 @@
+#for i in range(1,101):
+#    print('hola asir ', i)
+#print('fin')
+
+#for i in range(1,10):
+#    x = 4*i
+#    print('4 * ', i, ' = ', x)
+#print('fin')
+
+#x = int(input('introduce un numero'))
+#for i in range(1,10):
+#    z = i * x
+#    print(x, ' * ', i, ' = ', z)
+#print('fin')
+
+#for i in range(0,11,2):
+#    print(i)
+#print('fin')
+
+#x = 0
+#for i in range(0,101):
+#    x += i
+#    print(x)
+#print('fin')
+
+#for i in range (0,11):
+#   print(i)
+#    for j in range(0,11):
+#        z = i * j
+#        print(i, ' * ', j, ' = ', z)
+#print('fin')
+
+#x = int(input('introduce un numero '))
+#z = 1
+#for i in range(x, 0 , -1):
+#    print(i, ' * ', z)
+#    z *= i
+#    print(z)
+
+#print('el factorial es ', z)
+
